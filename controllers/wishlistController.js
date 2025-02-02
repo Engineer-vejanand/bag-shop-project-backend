d27ffc05dade629cd405f968098abc430cca2ac7
Engineer-vejanand <vejanandgdv2003@gmail.com>
2025-02-02 21:45:49 +0530

    add backend controller feature for remove from wishlist

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -42,3 +42,39 @@ module.exports.addtoWishlist = async (req, res) => {
         res.status(500).json({ message: " Error adding to wishlist", error: error.message});
     }
 }
+
+module.exports.removeFromWishlists = async (req, res) => {
+    try {
+        const { productId } = req.body;
+        const userId = req.user._id;
+
+        if(!userId){
+            res.status(400).json({ message:"userid missing"});
+        }
+
+        if(!productId){
+            res.status(400).json({ message:"product id missing"});
+        }
+
+        const wishlist = await Wishlist.findOne({ user: userId });
+    
+        if (!wishlist) {
+          return res.status(404).json({ message: "Wishlist not found" });
+        }
+    
+        if (!wishlist.products.includes(productId)) {
+          return res.status(404).json({ message: "Product not found in wishlist" });
+        }
+    
+        wishlist.products = wishlist.products.filter(
+          (product) => product.toString() !== productId
+        );
+    
+        await wishlist.save();
+
+        res.status(201).json({ message:"produt removing from wishlist"})
+        
+    } catch (error) {
+        res.status(500).json({ message:"server error",error:error.message});
+    }
+}
