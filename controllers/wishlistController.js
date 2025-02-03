27234b9764fc91af3911272db7da78eedd49b7be
Engineer-vejanand <vejanandgdv2003@gmail.com>
2025-02-03 23:17:33 +0530

    add a getwishlist controller

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -78,3 +78,29 @@ module.exports.removeFromWishlists = async (req, res) => {
         res.status(500).json({ message:"server error",error:error.message});
     }
 }
+
+module.exports.getWishlist = async (req, res) => {
+    try {
+        // const { userId } = req.params;
+        const userId = req.user.id
+    
+        if (!userId ) {
+          return res.status(400).json({ message: " missing userId" });
+        }
+        if ( !mongoose.Types.ObjectId.isValid(userId)) {
+          return res.status(400).json({ message: "Invalid userId" });
+        }
+    
+        const wishlist = await Wishlist.findOne({ user: userId }).populate(
+          "products"
+        );
+    
+        if (!wishlist) {
+          return res.status(404).json({ message: "Wishlist not found" });
+        }
+    
+        res.status(201).json({ wishlist }) 
+      } catch (error) {
+        res.status(500).json({ message: "Error fetching wishlist", error: error.message });
+      }
+}
