cc7d9759f00dd1bb3aaa0cd6c7b50d402d8e3207
Engineer-vejanand <vejanandgdv2003@gmail.com>
2025-02-09 21:55:09 +0530

    add a feature of get category by id

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,6 @@
-const Category = "../models/category.model.js";
+const { Category } = require("../models/category.model.js");
+const { Product } = require("../models/product.model.js");
+const { Wishlist } = require("../models/wishlist.model.js");
 
 module.exports.createCategory = async (req, res) => {
     try {
@@ -44,4 +46,25 @@ try {
 } catch (error) {
     res.status(500).json({message:"Error fetching categories",error:error.message});
 }
+}
+
+module.exports.getCategoryById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const category = await Category.findById(id).populate('parentCategory', 'name');
+
+        if(!category){
+            return res.status(404).json({ message: "category not found"});
+        };
+
+        const products = await Product.find({ category: id}).populate("category","name");
+        const wishlist = await Wishlist.findOne({ user: req.user._id }).populate("products");
+        const likedProducts = wishlist ? wishlist.products.map(product => product._id.toString()) :[];
+
+        res.status(201).json({ message:"success", category, products, likedProducts});
+        
+    } catch (error) {
+        res.status(500).json({ message:"error in getcategory",error:error.message});
+    }
 }
