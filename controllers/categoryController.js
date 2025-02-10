2e39079aac53e1a631d20bc06377298337b57239
Engineer-vejanand <vejanandgdv2003@gmail.com>
2025-02-10 22:52:47 +0530

    add a delete category feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -68,3 +68,28 @@ module.exports.getCategoryById = async (req, res) => {
         res.status(500).json({ message:"error in getcategory",error:error.message});
     }
 }
+
+module.exports.deleteCategory = async (req, res) => {
+    try {
+        const { id } = req.params;
+  
+        const category = await Category.findById(id);
+  
+        if(!category){
+           return res.status(404).json({ message:"Category not found"});
+        }
+  
+        const childCategories = await Category.find({ parentCategory: id});
+  
+        if(childCategories.length > 0){
+           return res.status(400).json({ message: " cannot delete category with subcategories. Reassign or delete them first."});
+        };
+  
+        await Category.findByIdAndDelete(id);
+  
+        res.status(200).json({message: " Category deleted successfully"});
+  
+     } catch (error) {
+        return res.status(500).json({ message:"Error fetching delete category",error: error.message});
+     }
+  }
